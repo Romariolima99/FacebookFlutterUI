e5bbda590e36244ea9b1a80d77dc5e256fd08f27
Romário Lima <devromariolima@gmail.com>
2024-08-05 16:54:27 -0300

    Create: Criação da Barra de navegação responsiva

--- lib/telas/principal.dart
+++ lib/telas/principal.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:my_flutter_web_app/componentes/navegacao_abas.dart';
+import 'package:my_flutter_web_app/componentes/navegacao_abas_desktop.dart';
 import 'package:my_flutter_web_app/telas/home.dart';
+import 'package:my_flutter_web_app/uteis/responsivo.dart';
 
 class Principal extends StatefulWidget {
   const Principal({super.key});
@@ -51,13 +53,26 @@ int _indiceAbaSelecionada = 0;
 
   @override
   Widget build(BuildContext context) {
+
+    final bool isDesktop = Responsivo.isDesktop(context);
+    Size tamanho = MediaQuery.of(context).size;
+
     return DefaultTabController(
       length: _icones.length, 
       child: Scaffold(
+        appBar: isDesktop 
+         ? PreferredSize(
+          preferredSize: Size(tamanho.width , 100),
+          child: NavegacaoAbasDesktop(),
+          )
+         : null,
+
         body:TabBarView(
           children: _telas,
         ),
-          bottomNavigationBar: NavegacaoAbas(
+          bottomNavigationBar: isDesktop 
+          ? null
+          : NavegacaoAbas(
             icones: _icones,
             indiceAbaSelecionada: _indiceAbaSelecionada,
             onTap: (indice){
