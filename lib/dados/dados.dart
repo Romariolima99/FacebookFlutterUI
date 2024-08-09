1725a35ab0290646c58097ad531926edfe8a7751
Romário Lima <devromariolima@gmail.com>
2024-08-08 22:46:03 -0300

    Create: criação das imagens da area de postagem

--- lib/dados/dados.dart
+++ lib/dados/dados.dart
@@ -15,43 +15,43 @@ import 'package:my_flutter_web_app/modelos/modelos.dart';
 
 /* Usuário Logado */
 Usuario usuarioAtual = Usuario(
-    nome: "Jamilton Damasceno",
-    urlImagem: "https://images.unsplash.com/photo-1498050108023-c5249f4df085"
+    nome: "Romário Lima",
+    urlImagem: "https://i.imgur.com/SR4R4Mc.jpg"
 );
 
 /* Lista de usuários online */
 List<Usuario> usuariosOnline = [
   Usuario(
-      nome: "José Renato de almeida",
-      urlImagem: "https://source.unsplash.com/UJLAMjEjISo/800x600"
+      nome: "Mauro Moura",
+      urlImagem: "https://i.imgur.com/xrghQtZ.jpg"
   ),
   Usuario(
-      nome: "Maria Almeida",
-      urlImagem: "https://source.unsplash.com/0wRXAXqIp58/800x600"
+      nome: "Michael Heming",
+      urlImagem: "https://i.imgur.com/HjibMuy.jpg"
   ),
   Usuario(
-      nome: "Fernando Lima de albuquerque filho",
-      urlImagem: "https://source.unsplash.com/59HOrlAKTOU/800x600"
+      nome: "Jean Zahn",
+      urlImagem: "https://i.imgur.com/H9H4FJQ.jpg"
   ),
   Usuario(
-      nome: "Marcela Faria",
-      urlImagem: "https://source.unsplash.com/GXzHGgzraHc/800x600"
+      nome: "Gabriel Marcato",
+      urlImagem: "https://i.imgur.com/maTiRbk.jpg"
   ),
   Usuario(
-      nome: "Carlos Eduardo",
-      urlImagem: "https://source.unsplash.com/y8yimL21z8s/800x600"
+      nome: "João Vitor",
+      urlImagem: "https://i.imgur.com/QqF72xG.jpg"
   ),
   Usuario(
-      nome: "Joana Santos",
-      urlImagem: "https://source.unsplash.com/PXWiHU7pbeg/800x600"
+      nome: "Wilhan Azevedo",
+      urlImagem: "https://i.imgur.com/VRtTQYZ.jpg"
   ),
   Usuario(
-      nome: "Rodrigo Caio",
-      urlImagem: "https://source.unsplash.com/MS0RHQ1enek/800x600"
+      nome: "Vinicius Dal zot",
+      urlImagem: "https://i.imgur.com/YU7mqL0.jpg"
   ),
   Usuario(
-      nome: "Pedro do Carmo",
-      urlImagem: "https://source.unsplash.com/9ChfwZzv4ys/800x600"
+      nome: "Johnny hoffmann",
+      urlImagem: "https://i.imgur.com/hCSdKRm.jpg"
   ),
 ];
 
@@ -97,7 +97,7 @@ List<Estoria> estorias = [
 List<Postagem> postagens = [
   Postagem(
       usuario: usuariosOnline[0],
-      descricao: "Passeio muito legal no final de semana",
+      descricao: "caramba, olha este site que o Romário desenvolveu!",
       tempoAtras: "20m",
       urlImagem: "https://i.imgur.com/OveOnVX.png",
       curtidas: 30,
@@ -108,7 +108,7 @@ List<Postagem> postagens = [
       usuario: usuariosOnline[1],
       descricao: "Quero compartilhar com você algo que aconteceu...",
       tempoAtras: "40m",
-      urlImagem: "https://source.unsplash.com/G0H58Z5aPog/800x600",
+      urlImagem: "https://i.imgur.com/WnA8maY.png",
       curtidas: 30,
       comentarios: 3,
       compartilhamentos: 5
@@ -117,16 +117,16 @@ List<Postagem> postagens = [
       usuario: usuariosOnline[2],
       descricao: "Não recomendo esse lugar, não fomos bem atendidos",
       tempoAtras: "55m",
-      urlImagem: "https://source.unsplash.com/1a_u4n02YNo/800x600",
+      urlImagem: "https://i.imgur.com/NVk8Cwu.png",
       curtidas: 30,
       comentarios: 3,
       compartilhamentos: 5
   ),
   Postagem(
       usuario: usuariosOnline[3],
-      descricao: "Não importa o que você sabe, mas o que faz com o que você sabe!",
+      descricao: "Este mês batemos todos os recordes de vendas!",
       tempoAtras: "1h",
-      urlImagem: "https://source.unsplash.com/sfL_QOnmy00/800x600",
+      urlImagem: "https://i.imgur.com/vNg6oZx.jpg",
       curtidas: 30,
       comentarios: 3,
       compartilhamentos: 5
@@ -135,7 +135,7 @@ List<Postagem> postagens = [
       usuario: usuariosOnline[4],
       descricao: "Preciso de indicações de eletricista, meu chuveiro pifou :(",
       tempoAtras: "2h",
-      urlImagem: "https://source.unsplash.com/_tWMmC9CQXQ/800x600",
+      urlImagem: "https://i.imgur.com/jne2SOw.jpg",
       curtidas: 30,
       comentarios: 3,
       compartilhamentos: 5
@@ -144,29 +144,25 @@ List<Postagem> postagens = [
       usuario: usuariosOnline[5],
       descricao: "Que chuva maravilhosa!",
       tempoAtras: "2d",
-      urlImagem: "https://source.unsplash.com/a4wUKaaMGWQ/800x600",
+      urlImagem: "https://i.imgur.com/Xve2k0T.jpg",
       curtidas: 30,
       comentarios: 3,
       compartilhamentos: 5
   ),
   Postagem(
       usuario: usuariosOnline[6],
-      descricao: "Um dia mais que especial, nos divertimos bastante ;)",
+      descricao: "Este simulador é incrivel!",
       tempoAtras: "3d",
-      urlImagem: "https://source.unsplash.com/YS1OOmHAFD0/800x600",
+      urlImagem: "https://i.imgur.com/Z8M3Xlu.jpg",
       curtidas: 100,
       comentarios: 30,
       compartilhamentos: 3
   ),
   Postagem(
       usuario: usuariosOnline[7],
-      descricao: "VOCÊ É DO TAMANHO DOS SEUS SONHOS! Lute, persista, "
-          "insista, corra atrás, passe algumas noites sem dormir direito, "
-          "mas nunca desista de seus sonhos, acredite que é capaz, se olhe "
-          "no espelho e diga pra você: VOCÊ TEM POTENCIAL! "
-          "Ninguém além de você mesmo pode torná-lo real.",
+      descricao: "Aprendi a arte da pesca com o Romário, o cara é do bone virado!",
       tempoAtras: "3d",
-      urlImagem: "https://source.unsplash.com/Ng2Hg1YMMuU/800x600",
+      urlImagem: "https://i.imgur.com/uoG4pjp.jpg",
       curtidas: 230,
       comentarios: 25,
       compartilhamentos: 3
